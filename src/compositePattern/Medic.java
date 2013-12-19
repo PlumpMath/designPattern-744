a809302f6ec64ad1f08b11c780d9175bb04d228a
kalkin2 <kalkin2@naver.com>
2013-12-19 10:42:51 +0900

    Medic Unit 추가

---
+++ src/compositePattern/Medic.java
@@ -0,0 +1,16 @@
+package compositePattern;
+
+/**
+* Created with IntelliJ IDEA.
+* User: guemseok.ju
+* Date: 13. 12. 19
+* Time: 오전 10:41
+* To change this template use File | Settings | File Templates.
+*/
+class Medic extends Unit {
+
+    @Override
+    void selectUnit() {
+        System.out.println("메딕이 선택 되었습니다");
+    }
+}
