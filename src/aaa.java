0af1cd287e51b72e5c631ebdc91bb6aed929088b
kalkin2 <kalkin2@naver.com>
2013-12-19 10:32:56 +0900

    ...

---
+++ src/aaa.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: guemseok.ju
+ * Date: 13. 12. 19
+ * Time: 오전 10:31
+ * To change this template use File | Settings | File Templates.
+ */
+public class aaa {
+}
